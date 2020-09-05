293d01eb2af6fa3da9931cabe4416cc3c5c9b521
didih <diedieh02@gmail.com>
2020-09-05 20:17:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pushnotification/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.pushnotification;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText ed1,ed2,ed3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ed1=(EditText)findViewById(R.id.editText);
+        ed2=(EditText)findViewById(R.id.editText2);
+        ed3=(EditText)findViewById(R.id.editText3);
+        Button b1=(Button)findViewById(R.id.button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String CHANNEL_ID = "my_channel_01";// The id of the channel.
+
+                String tittle=ed1.getText().toString().trim();
+                String subject=ed2.getText().toString().trim();
+                String body=ed3.getText().toString().trim();
+
+                NotificationManager notif=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+                Notification notify=new Notification.Builder
+                        (MainActivity.this).setContentTitle(tittle).setContentText(body).
+                        setContentTitle(subject).setSmallIcon(R.drawable.ic_launcher_background).setChannelId(CHANNEL_ID).build();
+
+                notify.flags |= Notification.FLAG_AUTO_CANCEL;
+                notif.notify(0, notify);
+            }
+        });
+    }
+}
